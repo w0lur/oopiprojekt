87f7d896cd40ff5f12c218485e0e7b4ea1648318
Georg Karu <Karu@Georgs-MacBook-Pro.local>
2019-03-16 16:00:44 +0200

    testfail1

---
+++ parkimiseprogramm/Auto.java
@@ -0,0 +1,12 @@
+package parkimiseprogramm;
+
+public class Auto {
+    private String regNr; //auto registreerimisnumber
+    private int parkimisAeg; //minutites
+
+    public Auto(String regNr, int parkimisAeg) {
+        this.regNr = regNr;
+        this.parkimisAeg = parkimisAeg;
+    }
+
+}
